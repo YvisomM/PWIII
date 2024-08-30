8dad821cae318e231b84a73e217b3f2c188d3c9b
Yvisom Manoel <yvisom2012@gmail.com>
2024-08-29 22:23:46 -0300

    Aula de Next

---
+++ aula01_nx/app/TESTE/page.js
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1>Olá Mundo</h1>
+      <br/>
+      <h2>AAAAAAAAAA</h2>
+    </main>
+  );
+}
---
+++ aula01_nx/app/page.js
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1 className="px-1 py-4">Olá Mundo</h1>
+      
+      <p>AAAAAAAAAA</p>
+    </main>
+  );
+}
